2ca3cff86a5a7cd191cbdcc24edec07db0599c8a
Tarook <Tarook.Zinou@gmail.com>
2022-12-16 23:37:28 +0100

    Added player movement
    pog gaming

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 5f;
+    [SerializeField] private Vector2 movement;
+    [SerializeField] private Vector2 mousePos;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(hor, ver);
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (movement.normalized * speed * Time.deltaTime));
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}

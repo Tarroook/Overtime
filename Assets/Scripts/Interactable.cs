c7c0af9d835c1a4f8588814546f6c101a0fcfd80
Tarook <Tarook.Zinou@gmail.com>
2022-12-17 21:41:49 +0100

    fixed requirement of collider2D for interactable

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Collider2D))]
 public abstract class Interactable : MonoBehaviour
 {
     public abstract void interact();

cfc0ff6401deffe6981c63c54a6e15214608f822
Tarook <Tarook.Zinou@gmail.com>
2022-12-17 16:00:19 +0100

    Merging map + enemies + interact + added icons to editor

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -2,21 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class interact : MonoBehaviour
+public class Interact : MonoBehaviour
 {
 
-    public List<GameObject> interactable = new List<GameObject>(); 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<GameObject> interactable = new List<GameObject>();
 
     private void OnTriggerEnter2D(Collider2D collision)
     {

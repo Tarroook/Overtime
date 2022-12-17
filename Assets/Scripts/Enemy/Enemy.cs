489ccc8783bc5648ca8eaf623c335f18dec76c33
Etoile2003 <bastien.pelletier01@gmail.com>
2022-12-17 21:08:02 +0100

    rework enemy

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy  : MonoBehaviour
 {
-    DetectPlayer zone;
-    Health hpScript;
-    public float speed = 3f;
-    bool isAwake = false;
-    GameObject player; 
-
+    protected DetectPlayer zone;
+    protected Health hpScript;
+    public float speed = 4f;
+    protected bool isAwake = false;
+    protected GameObject player;
+    protected Rigidbody2D rb; 
     private void OnEnable()
     {
         hpScript = gameObject.GetComponent<Health>();
@@ -23,37 +23,12 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         zone = gameObject.GetComponentInChildren<DetectPlayer>();
         player = GameObject.FindGameObjectWithTag("Player");        
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (player == null)
-            return;
-        if (!isAwake)
-        {
-            if (zone.awaike)
-            {
-                isAwake = true;
-            }
-
-        }else
-        {
-            //fonction poursuvre joueur 
-            chasePlayer();
-        }
-    }
-
-
-
-    void chasePlayer()
-    {
-        float step = speed * Time.deltaTime;
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, step);
-    }
-
 
 
     void damageTaken()
@@ -63,23 +38,6 @@ public class Enemy : MonoBehaviour
             isAwake = true;
         }
     }
-
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            collision.GetComponent<Health>().takeDamage(5);
-        }
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            collision.GetComponent<Health>().takeDamage(5);
-        }
-    }
-
 
 
 }

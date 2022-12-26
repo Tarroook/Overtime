aff92e59126c4b86cd1dc011a7b0ed8491a7c6a2
Tarook <Tarook.Zinou@gmail.com>
2022-12-26 09:48:04 +0100

    Text moves only stacking left to do

--- Assets/Scripts/ChooseRoomButton.cs
+++ Assets/Scripts/ChooseRoomButton.cs
@@ -13,7 +13,10 @@ public class ChooseRoomButton : MonoBehaviour
     public static event clickButtonAction onSelectedRoomButton;
 
     private GameObject layout;
+    private GameObject mainText;
     public int maxElementsPerRow = 3;
+
+    private List<GameObject> images;
 
     //int currentRow = 0;
 
@@ -28,7 +31,10 @@ public class ChooseRoomButton : MonoBehaviour
         button.onClick.AddListener(() => clickedButton());
         layout = transform.GetChild(1).gameObject;
         if (layout == null)
-            Debug.LogWarning("No vertical layout found in chooseRoomUI");
+            Debug.LogWarning("No layout found in chooseRoomUI : " + roomNB);
+        mainText = transform.GetChild(0).gameObject;
+        if (mainText == null)
+            Debug.LogWarning("No text found in chooseRoomUI : " + roomNB);
     }
 
     private void addMod(Modifier mod)
@@ -49,34 +55,13 @@ public class ChooseRoomButton : MonoBehaviour
             onSelectedRoomButton(roomNB - 1);
     }
 
-    void addImage(Sprite sprite)
-    {/*
-        HorizontalLayoutGroup horizontalLayout;
-        if (layout.transform.childCount == 0)
+    void addImage(Sprite sprite) // move text + stack
+    {
+        if(images == null)
         {
-            horizontalLayout = addNewRow().GetComponent<HorizontalLayoutGroup>();
+            images = new List<GameObject>();
+            mainText.GetComponent<RectTransform>().offsetMax = new Vector2(0, 60);
         }
-        else
-        {
-            horizontalLayout = layout.transform.GetChild(currentRow).GetComponent<HorizontalLayoutGroup>();
-
-            if (horizontalLayout.transform.childCount >= maxElementsPerRow)
-            {
-                currentRow++;
-                horizontalLayout = addNewRow().GetComponent<HorizontalLayoutGroup>();
-            }
-        }
-
-        GameObject element = new GameObject("Element");
-
-        RectTransform rectTransform = element.AddComponent<RectTransform>();
-        rectTransform.sizeDelta = new Vector2(100, 100);
-
-        Image image = element.AddComponent<Image>();
-        image.sprite = sprite;
-
-        element.transform.SetParent(horizontalLayout.transform, false);
-        */
         GameObject element = new GameObject("Image");
 
         RectTransform rectTransform = element.AddComponent<RectTransform>();
@@ -85,22 +70,5 @@ public class ChooseRoomButton : MonoBehaviour
         image.sprite = sprite;
 
         element.transform.SetParent(layout.transform, false);
-    }
-
-    GameObject addNewRow()
-    {
-        GameObject row = new GameObject("Row");
-
-        RectTransform rectTransform = row.AddComponent<RectTransform>();
-
-        HorizontalLayoutGroup layoutGroup = row.AddComponent<HorizontalLayoutGroup>();
-        layoutGroup.childAlignment = TextAnchor.MiddleCenter;
-        layoutGroup.childControlHeight = true;
-        layoutGroup.childForceExpandHeight = true;
-        layoutGroup.childControlWidth = false;
-        layoutGroup.childForceExpandWidth = false;
-        row.transform.SetParent(layout.transform, false);
-
-        return row;
     }
 }
